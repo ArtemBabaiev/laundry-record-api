7c0718982a9784b1c5fa1686de775bf3cd7d23b3
ArtemBabaiev <artdcxx4@gmail.com>
2024-10-09 14:29:44 +0300

    inital commit

---
+++ src/main/java/com/undefined/laundry/LaundryRecordApiApplication.java
@@ -0,0 +1,13 @@
+package com.undefined.laundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryRecordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryRecordApiApplication.class, args);
+	}
+
+}
