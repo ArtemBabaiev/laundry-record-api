f25c1a4c094476f4c018c623a60933bf75f965cc
ArtemBabaiev <artdcxx4@gmail.com>
2024-10-23 15:51:38 +0300

    datetime validation

--- src/main/java/com/undefined/laundry/model/request/AddEntryRequest.java
+++ src/main/java/com/undefined/laundry/model/request/AddEntryRequest.java
@@ -3,9 +3,8 @@ package com.undefined.laundry.model.request;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import com.undefined.laundry.utils.annotation.TodayOrFuture;
-
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -28,9 +27,17 @@ public class AddEntryRequest {
 	private LocalTime time;
 
 	@NotNull
-	@TodayOrFuture
 	private LocalDate date;
 
 	@NotBlank
 	private String room;
+
+	@AssertTrue(message = "Provided date time should be future")
+	private boolean isDateTimeValid() {
+		if (this.date.isBefore(LocalDate.now())) {
+			return false;
+		}
+		LocalTime nowTime = LocalTime.now();
+		return this.time.getHour() > nowTime.getHour();
+	}
 }
